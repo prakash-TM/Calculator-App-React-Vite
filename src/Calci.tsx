b51a762270656feff76dbce30c111e03047e5009
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2021-11-01 19:08:41 +0530

    ASS-24 uploaded the codes at first

---
+++ src/Calci.tsx
@@ -0,0 +1,72 @@
+import { Component } from "react";
+import "./Calci.css"
+
+class Calci extends Component<any, any> {
+   
+  constructor(props: any) {
+    super(props);
+    this.state = { num1: "", num2: "" ,symbol:"",out:""};
+ 
+  }
+ 
+  arthOperation = () => {
+      const{num1,num2,symbol}=this.state;
+      var N_num1=Number(num1);
+      var N_num2=Number(num2);
+    if(symbol==="+"){
+        var res =N_num1 + N_num2;
+        this.setState({out:res});
+    }
+    else if(symbol==="-"){
+        var res =N_num1 - N_num2;
+        this.setState({out:res});
+    }
+    else if(symbol==="x" || symbol==="*"){
+        var res =N_num1 * N_num2;
+        this.setState({out:res});
+    }
+    else if(symbol==="/"){
+        var res =N_num1 / N_num2;
+        this.setState({out:res});
+    }
+    else if(symbol==="%"){
+        var res =N_num1 + N_num2;
+        this.setState({out:res});
+    }
+   
+  };
+  render(){
+    var { num1, num2 ,symbol,out} = this.state;
+      return(
+          <>
+          <div className="mainContainer">
+          <input className="input" type="text" name="first number" placeholder="Enter the Number" value={num1} onChange={F_num => this.updateFValue(F_num)} />
+          <br />
+          <input className="input1" type="text" name="symbol" value={symbol}  onChange={SY => this.updateSYValue(SY)} /><span className="span">(*Operation Symbol)</span>
+          <br />
+          <input className="input" type="text" name="second number" value={num2} placeholder="Enter the Number" onChange={S_num => this.updateSValue(S_num)}/>
+          <br />
+          <button type="submit" onClick={this.arthOperation}>=</button>
+          <p>Result is : {out}</p>
+          </div>
+          </>
+      )
+  }
+  updateFValue(F_num:any) {
+    this.setState({
+      num1: F_num.target.value
+    });
+  }
+  updateSYValue(SY:any) {
+    this.setState({
+      symbol: SY.target.value
+    });
+  }
+  updateSValue(S_num:any) {
+    this.setState({
+      num2: S_num.target.value
+    });
+  }
+}
+
+export default Calci;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Calci from "./calci"
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+  <Calci/>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
